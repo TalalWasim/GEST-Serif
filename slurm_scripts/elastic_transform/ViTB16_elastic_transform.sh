8cc0b87ac6825efe294faa08b247c206f465f6d8
TalalWasim <wasimtalal@gmail.com>
2022-09-16 21:40:20 +0400

    add new slurm scripts

---
+++ slurm_scripts/elastic_transform/ViTB16_elastic_transform.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=vitb16_elastic_transform
+#SBATCH --partition=default-short
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=14900
+#SBATCH --gres=gpu:1
+
+python train.py \
+--model 'ViTB16' \
+--adv_folder '../datasets/CIFAR-10-C' \
+--adv_dataset 'elastic_transform' \
+--base_lr 0.01 \
+--epochs 40 \
+--batch_size 128 \
+--gamma 0.2 \
+--step 5 \
+--gpu 0 \
+--seed 50
---
+++ slurm_scripts/saturate/Resnet18_saturate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=resnet_saturate
+#SBATCH --partition=default-short
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=14900
+#SBATCH --gres=gpu:1
+
+python train.py \
+--model 'Resnet18' \
+--adv_folder '../datasets/CIFAR-10-C' \
+--adv_dataset 'saturate' \
+--base_lr 0.01 \
+--epochs 40 \
+--batch_size 128 \
+--gamma 0.2 \
+--step 5 \
+--gpu 0 \
+--seed 50
